89b03c1e2f78ddca7a0eed47f2bfea633c322e87
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2023-11-12 09:01:36 +0100

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Link } from "react-router-dom"
 import SearchParams from "./SearchParams"
+import Details from "./Details"
 
 function App() {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <BrowserRouter>
+        <header> 
+        <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+        { /* The :id means that this is a variable that is passed to the url. This id can be 
+          acessed inside of the details page.*/}
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+    </BrowserRouter>
     </div>
   )
     
